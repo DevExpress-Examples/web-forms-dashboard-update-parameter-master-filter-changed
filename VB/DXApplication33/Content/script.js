29da1619df5043d69a85ef9076c733f97a944238
DevExpressExampleBot <fakeEmail>
2026-01-30 04:36:04 +0400

    Source auto update [skip ci]

---
+++ VB/DXApplication33/Content/script.js
@@ -0,0 +1,17 @@
+﻿var dashboardControl;
+
+function onBeforeRender(s, e) {
+    dashboardControl = s.GetDashboardControl();
+    var viewerApiExtension = dashboardControl.findExtension('viewerApi');
+    if (viewerApiExtension)
+        viewerApiExtension.on('itemMasterFilterStateChanged', onItemMasterFilterStateChanged);
+}
+
+function onItemMasterFilterStateChanged(e) {
+    if (e.itemName == "gridDashboardItem1") {
+        var dashboardParameterDialogExtension = dashboardControl.findExtension('dashboardParameterDialog');
+        var parameters = dashboardParameterDialogExtension.getParameters();
+        var parameter1 = parameters.getParameterByName("OrderID");
+        parameter1.setValue(e.values[0][0]);
+    }
+}
